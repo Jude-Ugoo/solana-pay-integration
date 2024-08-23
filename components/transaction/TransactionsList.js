64a843240ea3566b883ec0e81d78cfce6ccc80c2
Jude-Ugoo <ugooeze0@gmail.com>
2024-08-23 04:03:34 +0100

    experimenting

---
+++ components/transaction/TransactionsList.js
@@ -0,0 +1,63 @@
+import { useMemo, useState } from "react";
+import TransactionDetailModal from "./TransactionDetailModal";
+import TransactionItem from "./TransactionItem";
+
+const TransactionsList = ({ transactions }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentTransactionID, setCurrentTransactionID] = useState(null);
+  //   const currentTransaction = useMemo(
+  //     () =>
+  //       transactions.find(
+  //         (transaction) => transaction.id === currentTransactionID
+  //       ),
+  //     [currentTransactionID]
+  //   );
+
+  const currentTransaction = useMemo(() => {
+    if (!transactions || transactions.length === 0) return null;
+    return (
+      transactions.find(
+        (transaction) => transaction.id === currentTransactionID
+      ) || null
+    );
+  }, [currentTransactionID, transactions]);
+
+  const toggleTransactionDetailModal = (value, transactionID) => {
+    setCurrentTransactionID(transactionID);
+    setModalOpen(value);
+  };
+
+  return (
+    <div>
+      <div className="bg-[#f6f6f6] pb-4 pt-10">
+        <p className="mx-auto max-w-3xl px-10 text-sm font-medium uppercase text-[#abafb2] xl:px-0">
+          Transactions
+        </p>
+      </div>
+      <div className="mx-auto max-w-3xl divide-y divide-gray-100 py-4 px-10 xl:px-0">
+        {Array.isArray(transactions) &&
+          transactions.map(
+            ({ id, to, amount, description, transactionDate }) => (
+              <TransactionItem
+                key={id}
+                id={id}
+                to={to}
+                description={description}
+                transactionDate={transactionDate}
+                amount={amount}
+                toggleTransactionDetailModal={toggleTransactionDetailModal}
+              />
+            )
+          )}
+
+        <TransactionDetailModal
+          modalOpen={modalOpen}
+          setModalOpen={setModalOpen}
+          currentTransaction={currentTransaction}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TransactionsList;
---
+++ hooks/cashapp.js
@@ -0,0 +1,120 @@
+import { useState, useEffect } from "react";
+import { getAvatarUrl } from "../functions/getAvatarUrl";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import { BigNumber } from "bignumber.js";
+
+export const useCashApp = () => {
+  const [avatar, setAvatar] = useState("");
+  const [userAddress, setUserAddress] = useState(
+    "11111111111111111111111111111111"
+  );
+  const [amount, setAmount] = useState(0);
+  const [receiver, setReceiver] = useState("");
+  const [transactionPurpose, setTransactionPurpose] = useState("");
+
+  const { connected, publicKey, sendTransaction } = useWallet();
+  const { connection } = useConnection();
+
+  const useLocalStroage = (stroageKey, fallbackState) => {
+    const [value, setValue] = useState(
+      JSON.parse(localStorage.getItem(stroageKey)) ?? fallbackState
+    );
+
+    useEffect(() => {
+      localStorage.setItem(stroageKey, JSON.stringify(value));
+    }, [value, setValue]);
+    return [value, setValue];
+  };
+
+  const [transacions, setTransactions] = useLocalStroage("transactions", []);
+
+  // Get Avatar based on the userAddress
+  useEffect(() => {
+    if (connected && publicKey) {
+      setAvatar(getAvatarUrl(publicKey.toString()));
+      setUserAddress(publicKey.toString());
+    }
+  }, [connected, publicKey]);
+
+  //? Create the transaction to send to our wallet and we sign it from there
+  const makeTransaction = async (fromWallet, toWallet, amount, reference) => {
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = clusterApiUrl(network);
+    const connection = new Connection(endpoint);
+
+    //? Get a recent blockhash to include in the transaction
+    const { blockhash } = await connection.getLatestBlockhash("finalized");
+
+    const transaction = new Transaction({
+      recentBlockhash: blockhash,
+      feePayer: fromWallet, // Buyer pays the transacion fee...
+    });
+
+    //? Create the instruction to send SOL from owner to receipent
+    const transferInstruction = SystemProgram.transfer({
+      fromPubkey: fromWallet,
+      toPubkey: toWallet,
+      lamports: amount.multipliedBy(LAMPORTS_PER_SOL).toNumber(),
+    });
+
+    transferInstruction.keys.push({
+      pubkey: reference,
+      isSigner: false,
+      isWritable: false,
+    });
+
+    transaction.add(transferInstruction);
+
+    return transaction;
+  };
+
+  //? Create function to RUN the transaction. This will be added to the button
+  const doTransaction = async ({ amount, receiver, transactionPurpose }) => {
+    try {
+      const fromWallet = publicKey;
+      const toWallet = new PublicKey(receiver);
+      const bnAmount = new BigNumber(amount);
+      const reference = Keypair.generate().publicKey;
+      const transacion = await makeTransaction(
+        fromWallet,
+        toWallet,
+        bnAmount,
+        reference
+      );
+
+      const txnHash = await sendTransaction(transacion, connection);
+      await connection.confirmTransaction(txnHash, "processed");
+      console.log(txnHash);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    }
+
+    // Create transaction history object
+  };
+
+  return {
+    connected,
+    publicKey,
+    avatar,
+    userAddress,
+    doTransaction,
+    amount,
+    setAmount,
+    receiver,
+    setReceiver,
+    transactionPurpose,
+    setTransactionPurpose,
+    transacions,
+    setTransactions,
+  };
+};
